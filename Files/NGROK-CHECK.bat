b0e00a2fdb1aba8ba304396cd2a5a9c30cf6ad0c
daggernet <81592659+daggernet@users.noreply.github.com>
2021-06-19 17:15:53 -0300

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user dagger dagger#rdp01 /add >nul
-net localgroup administrators administrator /add >nul
+net localgroup administrators dagger /add >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
@@ -19,8 +19,8 @@ PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComp
 diskperf -Y >nul
 sc start audiosrv >nul
 sc config Audiosrv start= auto >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
+ICACLS C:\Windows\Temp /grant dagger:F >nul
+ICACLS C:\Windows\installer /grant dagger:F >nul
 ping -n 10 127.0.0.1 >nul
 
 
